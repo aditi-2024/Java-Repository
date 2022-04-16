dc32a024f1ac199a4f7c0ce48ebcfa9ed6ed71ee
C2 <C2@DESKTOP-QDCN360>
2022-04-16 20:39:05 +0530

    Unit01_TasksInJava

---
+++ Learning Java/src/unit_01/P2_Task01_KeywordsInJava.java
@@ -0,0 +1,48 @@
+package unit_01;
+
+public class P2_Task01_KeywordsInJava {
+
+	 static int add(int a,int b)
+     {
+    	 System.out.println("In Function static");
+    	 return (a+b);
+     }
+	  int multi(int a, int b)
+	  {
+		  System.out.println("In Function non-static");
+	    	 return (a*b);
+	  }
+	public static void main(String[] args) 
+	{
+		System.out.println("P2_Task01_KeywordsInJava");
+		/*
+		 * Cannot used as token
+		 Here we cannot use token(double ) as variable name 
+		 
+		int double=3;
+        */
+		int a=3;
+		int b=4;
+		System.out.println(a);
+	     /*
+	     Here we cannot use this token(int) as function name
+		 void int()
+		 {
+			 System.out.println("Hello");
+		 }
+		 */
+         
+		int c=add(a,b);		
+		System.out.println(c);
+		 /* 
+		   Here we cannot use this token(int) as class  name
+		   
+		   ABC int =new ABC();
+		 */
+		P2_Task01_KeywordsInJava  obj = new P2_Task01_KeywordsInJava();
+		System.out.println(obj.multi(a, b));
+	}
+	
+	   
+	
+}
